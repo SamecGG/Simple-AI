22597e7be062aed842e94ba298ad8279a1976fa7
Sam <samuel.grulich@gmail.com>
2021-03-06 00:50:10 +0100

    multiple layers with mupltiple inputs
    Input batch size of 2, layers are hardcoded in

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-inputs = [1, 2]
+# 3 neurons => 3 outputs
+inputs = [[1, 2],
+          [3, 2]]
+
 weights = [[1, 2],
            [3, 4],
            [5, 6]]
+
 biases = [1, 2, 3]
 
-output = np.dot(weights, inputs) + biases
+weights2 = [[1, 2],
+            [3, 4],
+            [5, 6]]
+
+biases2 = [1, 2, 3]
+
+layer1_output = np.dot(inputs, np.array(weights).T) + biases
+layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases
+
+layer2_output
