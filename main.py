f9d6a4bb42fb46a8d86c80f87858377bc7f4f1c7
Sam <samuel.grulich@gmail.com>
2021-03-06 01:07:37 +0100

    Dense layer object

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-# 3 neurons => 3 outputs
-inputs = [[1, 2],
-          [3, 2]]
+np.random.seed(0)
 
-weights = [[1, 2],
-           [3, 4],
-           [5, 6]]
+X = [[1, 2, 5, 0],
+     [3, 2, 1, 3],
+     [1, 3, -1, 2]]
 
-biases = [1, 2, 3]
+class Dense_Layer:
+     def __init__(self, n_inputs, n_neurons):
+          self.weights = np.random.randn(n_inputs, n_neurons)
+          self.biases = np.zeros(1, n_neurons)
+     def forward(self, inputs):
+          self.output = np.dot(inputs, self.weights) + self.biases
 
-weights2 = [[1, 2],
-            [3, 4],
-            [5, 6]]
+layer1 = Dense_Layer(4, 5)
+layer2 = Dense_Layer(5, 2)
 
-biases2 = [1, 2, 3]
+layer1.forward(X)
+layer2.forward(layer1.output)
 
-layer1_output = np.dot(inputs, np.array(weights).T) + biases
-layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases
-
-layer2_output
+layer2.output
